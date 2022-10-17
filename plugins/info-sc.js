cd70026a59357804a788a1f127c406d8b61973eb
NKMODS <86371273+NKMODS@users.noreply.github.com>
2022-10-17 19:41:09 +0530

    🔥Zenitsu Bot
    Base Started!

--- plugins/info-sc.js
+++ plugins/info-sc.js
@@ -1,30 +1,56 @@
-let handler = async (m, { conn, usedPrefix }) => {
-var doc = ['pdf','zip','vnd.openxmlformats-officedocument.presentationml.presentation','vnd.openxmlformats-officedocument.spreadsheetml.sheet','vnd.openxmlformats-officedocument.wordprocessingml.document']
-var document = doc[Math.floor(Math.random() * doc.length)]    
-let texto1 = `*https://github.com/NKMODS/Zenitsu-Bot-MD*`
-let buttonMessage= {
-'document': { url: `https://github.com/NKMODS/Zenitsu-Bot-MD` },
-'mimetype': `application/${document}`,
-'fileName': `「  𝑯𝒆𝒍𝒍𝒐 𝑾𝒐𝒓𝒍𝒅 」`,
-'fileLength': 99999999999999,
-'pageCount': 200,
-'contextInfo': {
-'forwardingScore': 200,
-'isForwarded': true,
-'externalAdReply': {
-'mediaUrl': 'https://github.com/NKMODS/Zenitsu-Bot-MD',
-'mediaType': 2,
-'previewType': 'pdf',
-'title': 'ᴇʟ ᴍᴇᴊᴏʀ ʙᴏᴛ ᴅᴇ ᴡʜᴀᴛsᴀᴘᴘ⁩',
-'body': wm,
-'thumbnail': imagen1,
-'sourceUrl': 'https://www.youtube.com/channel/UCSTDMKjbm-EmEovkygX-lCA' }},
-'caption': texto1,
-'footer': wm,
-'buttons':[
-{buttonId: `${usedPrefix}menu`, buttonText: {displayText: '𝙼𝙴𝙽𝚄'}, type: 1}, 
-{buttonId: `${usedPrefix}donar`, buttonText: {displayText: '𝙳𝙾𝙽𝙰𝚁'}, type: 1}],
-'headerType': 6 }
-conn.sendMessage(m.chat, buttonMessage, { quoted: m })}
-handler.command = ['sc','script','botgit']
+import fs, { promises } from 'fs'
+import fetch from 'node-fetch'
+let handler = async (m, { conn, usedPrefix, usedPrefix: _p, __dirname, text }) => {
+try {
+let vn = './media/menu.mp3'
+let pp = imagen4
+let img = await(await fetch('https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png')).buffer()
+let d = new Date(new Date + 3600000)
+let locale = 'es'
+let week = d.toLocaleDateString(locale, { weekday: 'long' })
+let date = d.toLocaleDateString(locale, { day: 'numeric', month: 'long', year: 'numeric' })
+let _uptime = process.uptime() * 1000
+let uptime = clockString(_uptime)
+let rtotalreg = Object.values(global.db.data.users).filter(user => user.registered == true).length 
+let more = String.fromCharCode(8206)
+let readMore = more.repeat(850)   
+let taguser = '@' + m.sender.split("@s.whatsapp.net")[0]
+let str = `
+*${taguser}, Im Zenitsu Bot*
+_My Script:_ *https://github.com/NKMODS/Zenitsu-Bot-MD*
+_Creater_: *NKMODS*
+`.trim()
+let buttons = [
+{ buttonId: '#menu', buttonText: { displayText: 'Main Menu' }, type: 1 }]
+let buttonMessage = {
+image: pp,
+caption: str.trim(),
+mentions: [m.sender],
+footer: `*Zenitsu-Bot-MD Script by NKMODS*`,
+buttons: buttons,
+headerType: 4,
+contextInfo: {
+mentionedJid: [m.sender],
+externalAdReply: {
+showAdAttribution: true,
+mediaType: 'VIDEO',
+mediaUrl: null,
+title: 'ZENITSU - BOT MD',
+body: null,
+thumbnail: img,
+sourceUrl: `https://github.com/NKMODS/Zenitsu-Bot-MD`
+}}}
+conn.sendMessage(m.chat, buttonMessage, { quoted: m })
+//await conn.sendFile(m.chat, vn, 'menu.mp3', null, m, true, { type: 'audioMessage', ptt: true})
+} catch {
+conn.reply(m.chat, ' ', m)
+}}
+handler.command = /^(botgit|script|sc|zenitsubot|zenitsubotfile|zenitsubotlink|zenitsubotgithub|nkmodszenitsu|botlink)$/i
+handler.exp = 50
+handler.fail = null
 export default handler
+function clockString(ms) {
+let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
+let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
+let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
+return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')}
